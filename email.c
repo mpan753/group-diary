4c80480fe0988cfc2435a42a1a5a84bab6718021
mpan753 <mia@Mias-MacBook-Pro.local>
2014-08-30 17:34:16 +1000

    added a check email input functionality

--- email.c
+++ email.c
@@ -1,6 +1,6 @@
 /*
  * src/tutorial/email.c
- * modified by Lujie Wang & Pan Meng
+ * modified by Lujie Wang & Meng Pan
  *
  ******************************************************************************
   This file contains routines that can be bound to a Postgres backend and
@@ -11,8 +11,12 @@
 #include "postgres.h"
 
 #include "fmgr.h"
-#include "libpq/pqformat.h"		/* needed for send/recv functions */
+#include "libpq/pqformat.h"     /* needed for send/recv functions */
 
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include <regex.h>
 
 PG_MODULE_MAGIC;
 
@@ -21,7 +25,6 @@ typedef struct Email{
     char* domain;
 } Email;
 
-char *strlwr(char *);
 
 /*
  * Since we use V1 function calling convention, all these functions have
@@ -30,35 +33,26 @@ char *strlwr(char *);
  */
 Datum email_in(PG_FUNCTION_ARGS);
 Datum email_out(PG_FUNCTION_ARGS);
-<<<<<<< HEAD
 //Datum email_recv(PG_FUNCTION_ARGS);
 //Datum email_send(PG_FUNCTION_ARGS);
 //Datum email_add(PG_FUNCTION_ARGS);
+
+char *strlwr(char *);
+bool is_valid_email(char *);
+bool is_valid_local(char *);
+bool is_valid_domain(char *);
+bool is_name_part(char **);
+bool is_name_parts(char **);
+bool is_name_chars(char **);
+
 /*
-  Datum		complex_abs_lt(PG_FUNCTION_ARGS);
-  Datum		complex_abs_le(PG_FUNCTION_ARGS);
-  Datum		complex_abs_eq(PG_FUNCTION_ARGS);
-  Datum		complex_abs_ge(PG_FUNCTION_ARGS);
-  Datum		complex_abs_gt(PG_FUNCTION_ARGS);
-  Datum		complex_abs_cmp(PG_FUNCTION_ARGS);
+  Datum     complex_abs_lt(PG_FUNCTION_ARGS);
+  Datum     complex_abs_le(PG_FUNCTION_ARGS);
+  Datum     complex_abs_eq(PG_FUNCTION_ARGS);
+  Datum     complex_abs_ge(PG_FUNCTION_ARGS);
+  Datum     complex_abs_gt(PG_FUNCTION_ARGS);
+  Datum     complex_abs_cmp(PG_FUNCTION_ARGS);
 */
-=======
-Datum email_recv(PG_FUNCTION_ARGS);
-Datum email_send(PG_FUNCTION_ARGS);
-
-Datum email_abs_lt(PG_FUNCTION_ARGS);
-Datum email_abs_le(PG_FUNCTION_ARGS);
-Datum email_abs_eq(PG_FUNCTION_ARGS);
-Datum email_abs_ge(PG_FUNCTION_ARGS);
-Datum email_abs_gt(PG_FUNCTION_ARGS);
-Datum email_abs_ne(PG_FUNCTION_ARGS);
-
-Datum email_abs_same_domain(PG_FUNCTION_ARGS);
-Datum email_abs_not_same_domain(PG_FUNCTION_ARGS);
-
-Datum email_abs_cmp(PG_FUNCTION_ARGS);
-
->>>>>>> FETCH_HEAD
 
 
 /*****************************************************************************
@@ -74,15 +68,18 @@ email_in(PG_FUNCTION_ARGS) {
     char *domain = (char *) malloc(sizeof(char *));
     Email *result;
 
-    char *lowercase = strlwr(str);
+    char *email = strlwr(str);
 
-<<<<<<< HEAD
-    if (sscanf(lowercase, "%[_a-zA-Z0-9.]@%[_a-zA-Z0-9.]", local, domain) != 2)
-=======
-    if (sscanf(lowercase, "%[-a-zA-Z0-9.]@%[-a-zA-Z0-9.]", local, domain) != 2)
->>>>>>> FETCH_HEAD
+//    if (sscanf(email, "%[_a-zA-Z0-9.]@%[_a-zA-Z0-9.]", local, domain) != 2)
+//        ereport(ERROR, (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+//                 errmsg("invalid input syntax for email: \"%s\"", str)));
+
+    if ( !is_valid_email(email) ) {
         ereport(ERROR, (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                 errmsg("invalid input syntax for email: \"%s\"", str)));
+                errmsg("invalid input syntax for email: \"%s\"", str)));
+    }
+
+    sscanf(email, "%[-a-zA-Z0-9.]@%s", local, domain);
 
     result = (Email *) malloc(sizeof(Email));
     result->local = local;
@@ -90,43 +87,131 @@ email_in(PG_FUNCTION_ARGS) {
     PG_RETURN_POINTER(result);
 }
 
-<<<<<<< HEAD
-char *strlwr(char *string) {
-	size_t len = strlen(string);
+bool is_valid_email(char *email) {
+    char *local = (char *) malloc(sizeof(char *));
+    char *domain = (char *) malloc(sizeof(char *));
+    char *reg_exp_mail = "^[a-z0-9.-]+@[a-z0-9.-]+$";
+    regex_t regex;
+    int pre;
+    pre = regcomp(&regex,reg_exp_mail,1);
+    pre = regexec(&regex, email, 0, NULL, 0);
+    if(pre){ // incorrect
+        return false;
+    }
+    regfree (&regex);
 
-	char *email = malloc(sizeof(char *));
+    if ( sscanf(email, "%[-a-zA-Z0-9.]@%s", local, domain) != 2 ) {
+        return false;
+    }
 
-	for (int i = 0; i < len; ++i)
-	{
-		if (isalpha(string[i]))
-		{
-			email[i] = (tolower(string[i]));
-=======
-char* strlwr(char *string) {
-	int len = strlen(string);
-
-        /* Changed this line to "my style", since I am believing dynamic allocation for string
-         * and returning causes pointer error, which could be the issue. */
-	char *email = malloc(sizeof(char) * (len + 1));
-
-        int i;
-	for (i = 0; i < len; ++i) //Or use "i <= len" to copy the ending '\0'
-	{
-		if (isalpha(string[i]))
-		{
-			email[i] = tolower(string[i]);
->>>>>>> FETCH_HEAD
-
-		} else {
-			email[i] = string[i];
-		}
-	}
-<<<<<<< HEAD
-=======
-        email[len] = '\0'; //Or delete this line for using above method.
->>>>>>> FETCH_HEAD
-	return email;
+    if (!is_valid_local(local)) {
+        return false;
+    }
+    if(!is_valid_domain(domain)) {
+        return false;
+    }
+    return true;
 }
+
+char *strlwr(char *string) {
+    size_t len = strlen(string);
+
+    char *email = malloc(sizeof(char *));
+
+    for (int i = 0; i < len; ++i)
+    {
+        if (isalpha(string[i]))
+        {
+            email[i] = (tolower(string[i]));
+
+        } else {
+            email[i] = string[i];
+        }
+    }
+    return email;
+}
+
+bool is_valid_local(char *loc) {
+    if (!is_name_part(&loc)) {
+        printf("name_part_false\n");
+        return false;
+    }
+    if (!is_name_parts(&loc)){
+        printf("name_parts_false\n");
+        return false;
+    }
+    if (*loc != '\0') {
+        return false;
+    }
+    // printf("dada %s\n", loc);
+    return true;
+}
+
+bool is_valid_domain(char *dom) {
+    if ( !is_name_part(&dom) ) {
+        return false;
+    }
+    if ( (*dom) == '.' ) {
+        dom++;
+        if ( !is_name_part(&dom) ) {
+            return false;
+        }
+        if ( !is_name_parts(&dom) ) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    if (*dom != '\0') {
+        return false;
+    }
+    return true;
+}
+
+bool is_name_part(char **pt_to_str) {
+    if (!isalpha(*(*pt_to_str))) {
+        return false;
+    }
+    while (isalpha(*(*pt_to_str))) {
+        (*pt_to_str)++;
+    }
+    if (!is_name_chars(pt_to_str)) {
+        return false;
+    }
+    if ((*(*pt_to_str)) == '\0') {
+        return true;
+    }
+    return true;
+}
+
+bool is_name_parts(char **pt_to_str) {
+    if ( (*(*pt_to_str)) == '\0' ) {
+        return true;
+    }
+    if ( (*(*pt_to_str)) == '.' ) {
+        (*pt_to_str)++;
+        if ( !is_name_part(pt_to_str) ) {
+            return false;
+        }
+        if ( !is_name_parts(pt_to_str) ) {
+            return false;
+        }
+    }
+    return true; // should never reach here.
+}
+
+bool is_name_chars(char **pt_to_str) {
+    while (isalpha(*(*pt_to_str)) || isdigit(*(*pt_to_str)) || ((*(*pt_to_str)) == '-')) {
+        if ((*(*pt_to_str)) == '-') {
+            if (*((*pt_to_str)+1) == '\0') {
+                return false;
+            }
+        }
+        (*pt_to_str)++;
+    }
+    return true;
+}
+// end of check
 
 PG_FUNCTION_INFO_V1(email_out);
 
@@ -135,12 +220,11 @@ email_out(PG_FUNCTION_ARGS) {
     Email *email = (Email *) PG_GETARG_POINTER(0);
     char *result;
 
-    result = (char *) palloc(257);
-    snprintf(result, 257, "%s@%s", email->local, email->domain);
+    result = (char *) palloc(100);
+    snprintf(result, 100, "(%s@%s)", email->local, email->domain);
     PG_RETURN_CSTRING(result);
 }
 
-<<<<<<< HEAD
 ///*****************************************************************************
 // * Binary Input/Output functions
 // *
@@ -151,7 +235,7 @@ email_out(PG_FUNCTION_ARGS) {
 //
 //Datum
 //email_recv(PG_FUNCTION_ARGS) {
-//    StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
+//    StringInfo    buf = (StringInfo) PG_GETARG_POINTER(0);
 //    Email *result;
 //
 //    result = (Email *) palloc(sizeof(Email));
@@ -164,13 +248,13 @@ email_out(PG_FUNCTION_ARGS) {
 //
 //Datum
 //email_send(PG_FUNCTION_ARGS) {
-//	Email *email = (Email *) PG_GETARG_POINTER(0);
-//	StringInfoData buf;
+//  Email *email = (Email *) PG_GETARG_POINTER(0);
+//  StringInfoData buf;
 //
-//	pq_begintypsend(&buf);
-//	pq_sendstring(&buf, email->local);
-//	pq_sendstring(&buf, email->domain);
-//	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+//  pq_begintypsend(&buf);
+//  pq_sendstring(&buf, email->local);
+//  pq_sendstring(&buf, email->domain);
+//  PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 //}
 
 ///*****************************************************************************
@@ -201,24 +285,24 @@ email_out(PG_FUNCTION_ARGS) {
 // * It's essential that the comparison operators and support function for a
 // * B-tree index opclass always agree on the relative ordering of any two
 // * data values.  Experience has shown that it's depressingly easy to write
-// * unintentionally inconsistent functions.	One way to reduce the odds of
+// * unintentionally inconsistent functions.    One way to reduce the odds of
 // * making a mistake is to make all the functions simple wrappers around
 // * an internal three-way-comparison function, as we do here.
 // *****************************************************************************/
 //
-//#define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
+//#define Mag(c)    ((c)->x*(c)->x + (c)->y*(c)->y)
 //
 //static int
 //complex_abs_cmp_internal(Complex * a, Complex * b)
 //{
-//	double		amag = Mag(a),
-//				bmag = Mag(b);
+//  double      amag = Mag(a),
+//              bmag = Mag(b);
 //
-//	if (amag < bmag)
-//		return -1;
-//	if (amag > bmag)
-//		return 1;
-//	return 0;
+//  if (amag < bmag)
+//      return -1;
+//  if (amag > bmag)
+//      return 1;
+//  return 0;
 //}
 //
 //
@@ -227,10 +311,10 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_lt(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
+//  PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
 //}
 //
 //PG_FUNCTION_INFO_V1(complex_abs_le);
@@ -238,10 +322,10 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_le(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
+//  PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
 //}
 //
 //PG_FUNCTION_INFO_V1(complex_abs_eq);
@@ -249,10 +333,10 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_eq(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
+//  PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
 //}
 //
 //PG_FUNCTION_INFO_V1(complex_abs_ge);
@@ -260,10 +344,10 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_ge(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
+//  PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
 //}
 //
 //PG_FUNCTION_INFO_V1(complex_abs_gt);
@@ -271,10 +355,10 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_gt(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
+//  PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
 //}
 //
 //PG_FUNCTION_INFO_V1(complex_abs_cmp);
@@ -282,187 +366,8 @@ email_out(PG_FUNCTION_ARGS) {
 //Datum
 //complex_abs_cmp(PG_FUNCTION_ARGS)
 //{
-//	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-//	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+//  Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+//  Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 //
-//	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
+//  PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
 //}
-=======
-/*****************************************************************************
- * Binary Input/Output functions
- *
- * These are optional.
- *****************************************************************************/
-
-PG_FUNCTION_INFO_V1(email_recv);
-
-Datum
-email_recv(PG_FUNCTION_ARGS) {
-    StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
-    Email *result;
-
-    result = (Email *) palloc(sizeof(Email));
-    result->local = pq_getmsgstring(buf);
-    result->domain = pq_getmsgstring(buf);
-    PG_RETURN_POINTER(result);
-}
-
-PG_FUNCTION_INFO_V1(email_send);
-
-Datum
-email_send(PG_FUNCTION_ARGS) {
-	Email *email = (Email *) PG_GETARG_POINTER(0);
-	StringInfoData buf;
-
-	pq_begintypsend(&buf);
-	pq_sendstring(&buf, email->local);
-	pq_sendstring(&buf, email->domain);
-	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
-}
-
-/*****************************************************************************
- * New Operators
- *
- * A practical Email datatype would provide much more than this, of course.
- *****************************************************************************/
-
-/* Something should be here? */
-
-
-/*****************************************************************************
- * Operator class for defining B-tree index
- *
- * It's essential that the comparison operators and support function for a
- * B-tree index opclass always agree on the relative ordering of any two
- * data values.  Experience has shown that it's depressingly easy to write
- * unintentionally inconsistent functions.	One way to reduce the odds of
- * making a mistake is to make all the functions simple wrappers around
- * an internal three-way-comparison function, as we do here.
- *****************************************************************************/
-
-static int
-internal_same_domain(Email *a, Email *b) {
-    if (!strcmp(a->local, b->local))
-        return 1;
-}
-
-
-static int
-email_abs_cmp_internal(Email *a, Email *b) {
-    if (strcmp(a->domain, b->domain) > 0)
-        return 1;
-    else if (strcmp(a->domain, b->domain) < 0)
-        return -1;
-    else {
-        if (!strcmp(a->local, b->local))
-            return 0;
-        else if(strcmp(a->local, b->local) > 0)
-            return 1;
-        else
-            return -1;
-    }
-}
-
-
-PG_FUNCTION_INFO_V1(email_abs_lt);
-
-Datum
-email_abs_lt(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) < 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_le);
-
-Datum
-email_abs_le(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) <= 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_eq);
-
-Datum
-email_abs_eq(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) == 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_ne);
-
-Datum
-email_abs_ne(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) != 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_ge);
-
-Datum
-email_abs_ge(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) >= 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_gt);
-
-Datum
-email_abs_gt(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(email_abs_cmp_internal(a, b) > 0);
-}
-
-PG_FUNCTION_INFO_V1(email_abs_cmp);
-
-Datum
-email_abs_cmp(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_INT32(email_abs_cmp_internal(a, b));
-}
-
-PG_FUNCTION_INFO_V1(email_abs_same_domain);
-
-Datum
-email_abs_same_domain(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(internal_same_domain(a, b));
-}
-
-PG_FUNCTION_INFO_V1(email_abs_not_same_domain);
-
-Datum
-email_abs_not_same_domain(PG_FUNCTION_ARGS) {
-    Email *a = (Email *) PG_GETARG_POINTER(0);
-    Email *b = (Email *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(!internal_same_domain(a, b));
-}
-
-/*
-void string_to_lower(char* string) {
-    int i = 0;
-    while(string[i] != '\0'){
-        string[i] = tolower(string[i]);
-        ++i;
-    }
-    return 0;
-}
-
-*/
->>>>>>> FETCH_HEAD
